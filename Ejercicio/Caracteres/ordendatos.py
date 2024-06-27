341235ea29e9f2b2e4851f8fbc1c5a6fde4dac50
brianherna <148389821+brianherna@users.noreply.github.com>
2024-06-27 16:18:43 +0200

    Delete Ejercicio directory

--- Ejercicio/Caracteres/ordendatos.py
+++
@@ -1,3 +0,0 @@
-# Función para ordenar los datos por longitud de cadena
-def ordenar_datos(datos):
-    return sorted(datos, key=lambda x: len(x))
--- Ejercicio/Caracteres/relativa.py
+++
@@ -1,3 +0,0 @@
-def frecuencia_relativa(frec_abs, total):
-    frec_rel = [round(abs / total, 2) for abs in frec_abs]
-    return frec_rel
